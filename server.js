99b00273ee60fcd2bcb1d11c8bdf4b2a62d54e32
joko triyanto <jokotriyanto123@outlook.com>
2021-12-24 12:55:08 +0700

    add api upload

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const fileUpload = require('express-fileupload');
 
 var app = express();
+
+app.use(fileUpload());
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,17 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', (req, res) => {
+  let sampleFile = req.files.upfile;
+  let fileName = sampleFile.name;
+  let fileSize = sampleFile.size;
+  let fileType = sampleFile.mimetype;
+  res.send({
+    name: fileName,
+    size: fileSize,
+    type: fileType
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
